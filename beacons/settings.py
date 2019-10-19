d833903d2bfef689973c26f2aed20ca48a1cd1a4
Ben Friedland <ben174@gmail.com>
2019-10-19 13:59:50 -0700

    fix static

--- beacons/settings.py
+++ beacons/settings.py
@@ -114,8 +114,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
-
-STATIC_URL = '/static/'
